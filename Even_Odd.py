d30cd4f14d23c3ec261da3b36e84309b10627af8
recipepa <72211263+recipepa@users.noreply.github.com>
2020-10-01 22:44:51 +0530

    Create Even_Odd.py

---
+++ Even_Odd.py
@@ -0,0 +1,14 @@
+print("--------------------Even or Odd Checker--------------")
+
+user = input("Enter a Number")
+
+if(user%2==0)
+print(user + "is even")
+
+elif(user%2!=0)
+print(user + "is odd")
+
+else
+print("!!!!invalid input!!!!")
+
+print("------------------Code runed-----------")
